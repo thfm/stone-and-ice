83190a5a0881ed57d4c0aa75dbf0412b695360f7
Thomas Freeman <tomclaudefreeman@gmail.com>
2020-03-24 13:32:09 +1100

    Add basic game mechanics

---
+++ Stone & Ice/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+    public Transform character;
+    public Vector3 followOffset;
+
+    void Update() {
+        transform.position = character.position + followOffset;
+    }
+}
---
+++ Stone & Ice/Assets/Scripts/Character/CharacterMovement.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour {
+    [Range(5, 50)] public float forwardsSpeed;
+    [Range(0.1f, 1)] public float sidewaysForce;
+
+    private Rigidbody rb;
+
+    void Start() {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate() {
+        rb.AddForce(Input.GetAxis("Horizontal") * sidewaysForce, 0, 0,
+            ForceMode.VelocityChange);
+
+        Vector3 velocity = rb.velocity;
+        velocity.z = forwardsSpeed;
+        rb.velocity = velocity;
+    }
+}
