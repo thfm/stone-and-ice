81ebfa18f5cd3d3f9f5b1bff6ec4b6c49a475036
Thomas Freeman <tomclaudefreeman@gmail.com>
2020-03-25 17:17:20 +1100

    Add simple menu scene

---
+++ Stone & Ice/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour {
+    public void LoadScene(string name) {
+        SceneManager.LoadScene(name);
+    }
+}
