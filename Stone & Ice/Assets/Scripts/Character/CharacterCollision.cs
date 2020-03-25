f4e1549c01686b1bff9dca52e3b199e2796fe7ae
Thomas Freeman <tomclaudefreeman@gmail.com>
2020-03-25 11:37:04 +1100

    Add character rotation and rotation reset

---
+++ Stone & Ice/Assets/Scripts/Character/CharacterCollision.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class CharacterCollision : MonoBehaviour {
+    private CharacterMovement movement;
+
+    void Start() {
+        movement = GetComponent<CharacterMovement>();
+    }
+
+    void OnCollisionEnter(Collision other) {
+        movement.sliding = (other.gameObject.tag == "IceFloor");
+    }
+}
--- Stone & Ice/Assets/Scripts/Character/CharacterMovement.cs
+++ Stone & Ice/Assets/Scripts/Character/CharacterMovement.cs
@@ -4,10 +4,29 @@ public class CharacterMovement : MonoBehaviour {
     [Range(5, 50)] public float forwardsSpeed;
     [Range(0.1f, 1)] public float sidewaysForce;
 
+    [Range(5f, 50f)] public float rotationSpeed;
+    [Range(0.5f, 10f)] public float rotationResetSpeed;
+
+    [HideInInspector] public bool sliding = false;
+
     private Rigidbody rb;
+    private float alignedRotation = 0;
 
     void Start() {
         rb = GetComponent<Rigidbody>();
+    }
+
+    void Update() {
+        CalibrateAlignedRotation();
+        if(sliding) {
+            transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime);
+        } else {
+            transform.rotation = Quaternion.Lerp(
+                transform.rotation,
+                Quaternion.Euler(0, alignedRotation, 0),
+                rotationResetSpeed * Time.deltaTime
+            );
+        }
     }
 
     void FixedUpdate() {
@@ -17,5 +36,9 @@ public class CharacterMovement : MonoBehaviour {
         Vector3 velocity = rb.velocity;
         velocity.z = forwardsSpeed;
         rb.velocity = velocity;
+    }
+
+    private void CalibrateAlignedRotation() {
+        alignedRotation = Mathf.Round(transform.eulerAngles.y / 90) * 90;
     }
 }
