a4cf60a58b170c19d4b3f45f35a2e1387d16cd0b
Thomas Freeman <tomclaudefreeman@gmail.com>
2020-03-26 16:26:01 +1100

    Change obstacle colour on collision; add moving obstacles

---
+++ Stone & Ice/Assets/Scripts/Obstacles/ObstacleCollision.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class ObstacleCollision : MonoBehaviour {
+    public Material hitMaterial;
+
+    void OnCollisionEnter(Collision other) {
+        if(other.gameObject.tag == "Character") {
+            GetComponent<Renderer>().material = hitMaterial;
+            ObstacleMovement movement = GetComponent<ObstacleMovement>();
+            if(movement != null) {
+                movement.enabled = false;
+            }
+        }
+    }
+}
---
+++ Stone & Ice/Assets/Scripts/Obstacles/ObstacleMovement.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class ObstacleMovement : MonoBehaviour {
+    public Vector3 motionOffset;
+    public float speed;
+
+    private Vector3 initialPosition;
+
+    void Start() {
+        initialPosition = transform.position;
+    }
+
+    void FixedUpdate() {
+        transform.position = Vector3.Slerp(
+            initialPosition,
+            initialPosition + motionOffset,
+            Mathf.PingPong(Time.timeSinceLevelLoad * speed, 1)
+        );
+    }
+}
