dbe2e7d7b8a1b8340f673a9bf038eac3122a15ad
Thomas Freeman <tomclaudefreeman@gmail.com>
2020-03-29 15:57:30 +1100

    Add progress indicator

---
+++ Stone & Ice/Assets/Scripts/UI/ProgressIndicator.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class ProgressIndicator : MonoBehaviour {
+    public Transform character;
+
+    private TextMeshProUGUI progressText;
+
+    void Start() {
+        progressText = GetComponent<TextMeshProUGUI>();
+    }
+
+    void Update() {
+        progressText.text = character.position.z.ToString("0");
+    }
+}
