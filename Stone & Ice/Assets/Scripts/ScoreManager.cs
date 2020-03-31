9255c8e9844cd17be7af7702a782a8d367e0e6b0
Thomas Freeman <tomclaudefreeman@gmail.com>
2020-03-31 20:14:50 +1100

    Refactor the score indication system and add a 'new best' indicator

---
+++ Stone & Ice/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class ScoreManager : MonoBehaviour {
+    public Transform character;
+
+    [HideInInspector] public int score = 0;
+    [HideInInspector] public static int highScore;
+
+    [HideInInspector] public bool newBest = false;
+
+    void Start() {
+        // highScore = PlayerPrefs.GetInt("highScore", 0);
+        highScore = 50;
+    }
+
+    void Update() {
+        score = (int) character.position.z;
+        if(score > highScore) {
+            highScore = score;
+            PlayerPrefs.SetInt("highScore", highScore);
+            newBest = true;
+        }
+    }
+}
---
+++ Stone & Ice/Assets/Scripts/UI/ScoreIndicator.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class ScoreIndicator : MonoBehaviour {
+    public ScoreManager scoreManager;
+    public TextMeshProUGUI distanceText;
+
+    private Animator animator;
+
+    void Start() {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update() {
+        distanceText.text = scoreManager.score.ToString();
+        if(scoreManager.newBest) {
+            animator.SetBool("NewBest", true);
+        }
+    }
+}
