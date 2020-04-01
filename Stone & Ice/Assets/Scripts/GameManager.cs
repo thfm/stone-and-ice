40ee9a1c91a01418c1393b573da109b0e6127cb6
Thomas Freeman <tomclaudefreeman@gmail.com>
2020-04-01 19:42:21 +1100

    Replace pause menu with black scene fade

--- Stone & Ice/Assets/Scripts/GameManager.cs
+++ Stone & Ice/Assets/Scripts/GameManager.cs
@@ -5,15 +5,12 @@ public class GameManager : MonoBehaviour {
 
     public SceneLoader sceneLoader;
     [Range(0.5f, 3)] public float restartDelay;
-    [Range(0.5f, 5)] public float menuReturnDelay;
 
     [Range(0.1f, 1)] public float musicFadeDuration;
 
-    public GameObject pauseMenu;
-    public KeyCode pauseKey;
+    public KeyCode returnToMenuKey;
 
     private bool gameOver = false;
-    private bool gamePaused = false;
 
     void Update() {
         if(character.transform.position.y < -1) {
@@ -26,36 +23,15 @@ public class GameManager : MonoBehaviour {
             gameOver = true;
         }
 
-        if(Input.GetKeyDown(pauseKey)) {
-            if(gamePaused) {
-                ResumeGame();
-            } else {
-                PauseGame();
-            }
+        if(Input.GetKeyDown(returnToMenuKey)) {
+            ReturnToMenu();
         }
-    }
-
-    public void PauseGame() {
-        Time.timeScale = 0;
-        pauseMenu.SetActive(true);
-        gamePaused = true;
-    }
-
-    public void ResumeGame() {
-        pauseMenu.SetActive(false);
-        Time.timeScale = 1;
-        gamePaused = false;
     }
 
     public void ReturnToMenu() {
         if(!gameOver) {
-            if(gamePaused) { ResumeGame(); }
-            Invoke("LoadMenuScene", menuReturnDelay);
+            StartCoroutine(sceneLoader.FadeToScene("Menu"));
             gameOver = true;
         }
-    }
-
-    private void LoadMenuScene() {
-        sceneLoader.LoadScene("Menu");
     }
 }
--- Stone & Ice/Assets/Scripts/SceneLoader.cs
+++ Stone & Ice/Assets/Scripts/SceneLoader.cs
@@ -1,12 +1,26 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SceneLoader : MonoBehaviour {
+    public RawImage blackFade;
+
     public void LoadScene(string name) {
         SceneManager.LoadScene(name);
     }
 
     public void ReloadCurrentScene() {
         LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public IEnumerator FadeToScene(string name) {
+        blackFade.GetComponent<Animator>().SetBool("FadeOut", true);
+        yield return new WaitUntil(() => blackFade.color.a == 1);
+        LoadScene(name);
+    }
+
+    public void ButtonFadeWrapper(string name) {
+        StartCoroutine(FadeToScene(name));
     }
 }
