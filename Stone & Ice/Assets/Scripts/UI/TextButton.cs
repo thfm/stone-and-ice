bbc7c0e26431e29e11008a3dd118f410ad306988
Thomas Freeman <tomclaudefreeman@gmail.com>
2020-04-05 15:43:57 +1000

    Replace default buttons with text buttons

---
+++ Stone & Ice/Assets/Scripts/UI/TextButton.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class TextButton : MonoBehaviour {
+    public Color hoverColour;
+
+    private TextMeshProUGUI text;
+    private Color normalColour;
+
+    void Start() {
+        text = GetComponentInChildren<TextMeshProUGUI>();
+        normalColour = text.color;
+    }
+
+    public void MouseEnter() {
+        text.color = hoverColour;
+    }
+
+    public void MouseExit() {
+        text.color = normalColour;
+    }
+}
