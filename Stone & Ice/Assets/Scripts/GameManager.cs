fb5673374007b3adbf268ba0090ffdadfd7f0ba1
Thomas Freeman <tomclaudefreeman@gmail.com>
2020-05-31 20:23:01 +1000

    Rearrange code structure and allow for game quitting

--- Stone & Ice/Assets/Scripts/GameManager.cs
+++ Stone & Ice/Assets/Scripts/GameManager.cs
@@ -1,42 +1,22 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    public GameObject character;
-
     public SceneLoader sceneLoader;
-    [Range(0.5f, 3)] public float restartDelay;
-
-    public KeyCode returnToMenuKey;
-
-    private bool gameOver = false;
 
     void Update()
     {
-        if (character.transform.position.y < -1)
+        if (Input.GetKeyDown(KeyCode.Escape))
         {
-            character.GetComponent<CharacterData>().isDead = true;
-        }
-
-        if (character.GetComponent<CharacterData>().isDead && !gameOver)
-        {
-            character.GetComponent<CharacterMovement>().enabled = false;
-            sceneLoader.Invoke("ReloadCurrentScene", restartDelay);
-            gameOver = true;
-        }
-
-        if (Input.GetKeyDown(returnToMenuKey))
-        {
-            ReturnToMenu();
-        }
-    }
-
-    public void ReturnToMenu()
-    {
-        if (!gameOver)
-        {
-            sceneLoader.LoadScene("Menu");
-            gameOver = true;
+            if (SceneManager.GetActiveScene().name == "Endless")
+            {
+                sceneLoader.LoadScene("Menu");
+            }
+            else
+            {
+                Application.Quit();
+            }
         }
     }
 }
---
+++ Stone & Ice/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    public GameObject character;
+
+    public SceneLoader sceneLoader;
+    [Range(0.5f, 3)] public float restartDelay;
+
+    private bool gameOver = false;
+
+    void Update()
+    {
+        if (character.transform.position.y < -1)
+        {
+            character.GetComponent<CharacterData>().isDead = true;
+        }
+
+        if (character.GetComponent<CharacterData>().isDead && !gameOver)
+        {
+            character.GetComponent<CharacterMovement>().enabled = false;
+            sceneLoader.Invoke("ReloadCurrentScene", restartDelay);
+            gameOver = true;
+        }
+    }
+}
